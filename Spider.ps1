5bf6f6a46b9eb44d25abf8527aafe743b0bd20f8
nathancarlmitchell <nat8754@hotmail.com>
2019-11-15 21:58:21 +0000

    Add files via upload

---
+++ Spider.ps1
@@ -0,0 +1,38 @@
+﻿$url = 'https://chfs.ky.gov/Pages/sitemap.aspx'
+
+$curl = curl $url
+
+$links = $curl.Links.href
+
+Set-Content -Path 'c:\users\nathan.mitchell\documents\chfsspider.txt' -Value 'CHFS Spider'
+
+foreach ($link in $links) {
+
+    if ($link.StartsWith('/')) {
+
+        $url = 'chfs.ky.gov' + $link
+
+        $curl = curl $url
+
+        $links = $curl.Links.href
+
+        foreach ($link in $links) {
+
+            if ($link.StartsWith('/')) {
+
+                $result = 'chfs.ky.gov' + $link
+
+                $result
+
+                Add-Content 'c:\users\nathan.mitchell\documents\chfsspider.txt' -Value $result
+
+            }
+
+        }
+
+    }
+
+}
+
+#check for unique values
+#ends with 'pdf'
