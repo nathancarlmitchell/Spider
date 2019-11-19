8b468999b7da78afda1144de6a9d558ba397b996
nathancarlmitchell <nat8754@hotmail.com>
2019-11-19 16:36:38 +0000

    Add files via upload

--- Spider.ps1
+++ Spider.ps1
@@ -1,82 +1,100 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$domain = 'technology.ky.gov'
-$url = 'https://technology.ky.gov/Pages/default.aspx'
+$domain = 'aba.ky.gov'
+$url = 'http://aba.ky.gov/Pages/default.aspx'
 $path =  'C:\Users\nathan.mitchell\Documents\Spider\'
 $file = $domain+'.txt'
+$level1 = 0
+$level2 = 0
+$level3 = 0
+$level4 = 0
+$level5 = 0
 
-Set-Content -Path $path$file -Value $url
+# Description $curl.links.innerText
+
+Set-Content -Path $path$file -Value 'URL,Depth'
 
 $curl = Invoke-WebRequest $url
 $links = $curl.Links.href
 
 foreach ($link in $links) {
-
-    $contentlink = $domain + $link
-
+    # Level 1
     if ($link.StartsWith('/')) {
-
+        $contentlink = $domain + $link
         if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
+            $level1++
             $result = $domain + $link
             $result
-            Add-Content -Path $path$file -Value $result
-        }
-
+            Add-Content -Path $path$file -Value $result',1' 
+        }    
         $url = $domain + $link
         $curl = Invoke-WebRequest $url
         $links = $curl.Links.href
-
         foreach ($link in $links) {
-
+            # Level 2
+            if ($link.StartsWith('/')) {
             $contentlink = $domain + $link
-
-            if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
-
-                if ($link.StartsWith('/')) {
-
+                if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
+                    $level2++
                     $result = $domain + $link
                     $result
-                    Add-Content -Path $path$file -Value $result
-
+                    Add-Content -Path $path$file -Value $result',2'
                     $url = $domain + $link
                     $curl = Invoke-WebRequest $url
                     $links = $curl.Links.href
-
                     foreach ($link in $links) {
-                        
-                        $contentlink = $domain + $link
-
-                        if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
-
-                            if ($link.StartsWith('/')) {
+                        # Level 3
+                        if ($link.StartsWith('/')) {
+                            $contentlink = $domain + $link
+                            if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
+                                $level3++
                                 $result = $domain + $link
                                 $result
-                                Add-Content -Path $path$file -Value $result
-
+                                Add-Content -Path $path$file -Value $result',3'
                                 $url = $domain + $link
                                 $curl = Invoke-WebRequest $url
                                 $links = $curl.Links.href
-
                                 foreach ($link in $links) {
-                        
-                                    $contentlink = $domain + $link
-            
-                                    if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
-            
-                                        if ($link.StartsWith('/')) {
+                                    # Level 4
+                                    if ($link.StartsWith('/')) {
+                                        $contentlink = $domain + $link
+                                        if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
+                                            $level4++
                                             $result = $domain + $link
                                             $result
-                                            Add-Content -Path $path$file -Value $result
+                                            Add-Content -Path $path$file -Value $result',4'
+                                            $url = $domain + $link
+                                            $curl = Invoke-WebRequest $url
+                                            $links = $curl.Links.href
+                                            foreach ($link in $links) {
+                                                # Level 5
+                                                if ($link.StartsWith('/')) {
+                                                    $contentlink = $domain + $link
+                                                    if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
+                                                        $level4++
+                                                        $result = $domain + $link
+                                                        $result
+                                                        Add-Content -Path $path$file -Value $result',5'
+                                                    }
+                                                }
+                                            }
                                         }
-                                    } else { 'Duplicate' }
+                                    }
                                 }
                             }
-                        } else { 'Duplicate' }
+                        }
                     }
                 }
-            } else { 'Duplicate' }
+            }
         }
     }
 }
+
+'Level 1: '+$level1
+'Level 2: '+$level2
+'Level 3: '+$level3
+'Level 4: '+$level4
+'Level 5: '+$level5
+
 #catch exceptions
 #check for unique values
 #ends with 'pdf'
