752b3d72520afb92fb551ea7144e483d6a4aa422
nathancarlmitchell <nat8754@hotmail.com>
2019-11-19 19:58:09 +0000

    Add files via upload

--- Spider.ps1
+++ Spider.ps1
@@ -1,20 +1,23 @@
 ﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$domain = 'aba.ky.gov'
-$url = 'http://aba.ky.gov/Pages/default.aspx'
-$path =  'C:\Users\nathan.mitchell\Documents\Spider\'
-$file = $domain+'.txt'
 $level1 = 0
 $level2 = 0
 $level3 = 0
 $level4 = 0
 $level5 = 0
-
+# remove 'domain + link' by using
+# $curl.BaseResponse.ResponseUri.AbsoluteUri
+$url = 'chfs.ky.gov'
+$path =  'C:\Users\nathan.mitchell\Documents\Spider\'
 # Description $curl.links.innerText
-
-Set-Content -Path $path$file -Value 'URL,Depth'
-
+# try
 $curl = Invoke-WebRequest $url
+# catch, log error with http code
 $links = $curl.Links.href
+$domain = $curl.BaseResponse.ResponseUri.Host
+# .txt vs .csv
+$file = $domain+'.txt'
+#Set-Content -Path $path$file -Value 'URL,Depth'
+Clear-Content -Path $path$file
 
 foreach ($link in $links) {
     # Level 1
@@ -23,11 +26,16 @@ foreach ($link in $links) {
         if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
             $level1++
             $result = $domain + $link
+            #$curl.BaseResponse.ResponseUri.AbsoluteUri
             $result
-            Add-Content -Path $path$file -Value $result',1' 
+            # commas in URL break CSV
+            Add-Content -Path $path$file -Value $result
+            #',1' 
         }    
+        #$curl.BaseResponse.ResponseUri.AbsoluteUri
         $url = $domain + $link
         $curl = Invoke-WebRequest $url
+        #$curl.links.innerText
         $links = $curl.Links.href
         foreach ($link in $links) {
             # Level 2
@@ -35,11 +43,15 @@ foreach ($link in $links) {
             $contentlink = $domain + $link
                 if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
                     $level2++
+                    #$curl.BaseResponse.ResponseUri.AbsoluteUri
                     $result = $domain + $link
                     $result
-                    Add-Content -Path $path$file -Value $result',2'
+                    Add-Content -Path $path$file -Value $result
+                    #',2'
+                    #$curl.BaseResponse.ResponseUri.AbsoluteUri
                     $url = $domain + $link
                     $curl = Invoke-WebRequest $url
+                    #$curl.links.innerText
                     $links = $curl.Links.href
                     foreach ($link in $links) {
                         # Level 3
@@ -49,7 +61,8 @@ foreach ($link in $links) {
                                 $level3++
                                 $result = $domain + $link
                                 $result
-                                Add-Content -Path $path$file -Value $result',3'
+                                Add-Content -Path $path$file -Value $result
+                                #',3'
                                 $url = $domain + $link
                                 $curl = Invoke-WebRequest $url
                                 $links = $curl.Links.href
@@ -61,7 +74,8 @@ foreach ($link in $links) {
                                             $level4++
                                             $result = $domain + $link
                                             $result
-                                            Add-Content -Path $path$file -Value $result',4'
+                                            Add-Content -Path $path$file -Value $result
+                                            #',4'
                                             $url = $domain + $link
                                             $curl = Invoke-WebRequest $url
                                             $links = $curl.Links.href
@@ -70,10 +84,11 @@ foreach ($link in $links) {
                                                 if ($link.StartsWith('/')) {
                                                     $contentlink = $domain + $link
                                                     if (!( Get-Content $path$file | Where-Object { $_.Contains($contentlink) } )) {
-                                                        $level4++
+                                                        $level5++
                                                         $result = $domain + $link
                                                         $result
-                                                        Add-Content -Path $path$file -Value $result',5'
+                                                        Add-Content -Path $path$file -Value $result
+                                                        #',5'
                                                     }
                                                 }
                                             }
@@ -96,5 +111,4 @@ foreach ($link in $links) {
 'Level 5: '+$level5
 
 #catch exceptions
-#check for unique values
 #ends with 'pdf'
