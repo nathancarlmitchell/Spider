57fa2ce9176f0bdb62b4f816e099f76ae665d4b6
nathancarlmitchell <nat8754@hotmail.com>
2020-01-31 15:36:15 -0500

    Add files via upload

--- Spider.py
+++ Spider.py
@@ -9,7 +9,13 @@ import time
 import requests
 from lxml import html
 from multiprocessing.pool import Pool
+#from util import replace_right
 
+
+def replace_right(source, target, replacement, replacements=None):
+    # >>> replace_right("asd.asd.asd.", ".", ". -", 1)
+    # 'asd.asd.asd. -'
+    return replacement.join(source.rsplit(target, replacements))
 
 def make_request(url):
     """Makes a web request, return links on the page."""
@@ -22,10 +28,13 @@ def make_request(url):
 
 
 def format_url(url):
-    # remove / # , from end
     url = url.replace('http://', '')
     url = url.replace('https://', '')
     url = url.replace(',', '%2C')
+    url = url.split('#')[0]
+    url = url.split('?')[0]
+    if url.endswith('/'):
+        url = replace_right(url, '/', '', 1)
     return url
 
 
@@ -67,9 +76,8 @@ def document_check(url):
 
 def main():
     start = time.time()
-    
-    #combine url and scope then add http://
-    url = 'http://usda.gov'
+    url = 'https://project-open-data.cio.gov/'
+    url = 'https://' + format_url(url)
     scope = format_url(url)
     unique = True
     depth = 0
@@ -93,6 +101,7 @@ def main():
                 try:
                     webpage = html.fromstring(request.content)
                     links = webpage.xpath('//a/@href')
+                    links = list(dict.fromkeys(links))
                     """Check if link is new and append to results."""
                     links = append_links(links, url, scope)
                     for link in links:
